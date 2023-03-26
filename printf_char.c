d3ba2b8b2f8226f16644deb48ea4a56ffcc9a895
ManNjoro <elijohnmwoho@gmail.com>
2023-03-26 07:07:17 -0700

    corrections

--- printf_char.c
+++ printf_char.c
@@ -1,12 +1,14 @@
 #include "main.h"
+
 /**
  * printf_char - prints a single character.
  * @args: an argument of type_valist.
- * Return: number of charaters printed.
+ * Return: number of characters printed.
  */
 int printf_char(va_list args)
 {
 	int c = va_arg(args, int);
 
-	return (_putchar(c));
+	_putchar(c);
+	return (1);
 }
