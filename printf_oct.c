4d4e527d3e123628ebf900162db0956ac542fe40
Joshua Akuna <akunajoshua@gmail.com>
2023-03-29 13:34:32 +0100

    hash flag implemented for print_oct

--- printf_oct.c
+++ printf_oct.c
@@ -14,27 +14,23 @@
 int printf_oct(va_list args, char *buffer, flg flags,
 		int width, int precision, int size)
 {
-	int i = 0, len = 0;
+	int i = 0, len = 0, pos = 0;
 	int *ptr;
 	unsigned long int _num, tmp;
 
 	_num = get_unsigned_num(args, size);
 	tmp = _num;
-
-	(void)buffer;
-	(void)flags;
 	(void)width;
 	(void)precision;
-
+	if (flags.hash)
+		buffer[pos++] = '0';
 	while (tmp / 8 != 0)
 	{
 		tmp /= 8;
 		len++;
 	}
 	len++;
-
 	ptr = malloc(len * sizeof(int));
-
 	while (i < len)
 	{
 		ptr[i++] = _num % 8;
@@ -42,9 +38,9 @@ int printf_oct(va_list args, char *buffer, flg flags,
 	}
 
 	for (i = len - 1; i >= 0; i--)
-		_putchar(ptr[i] + '0');
+		buffer[pos++] = ptr[i] + '0';
 
 	free(ptr);
 
-	return (len);
+	return (write(1, buffer, pos));
 }
